09358a47f4bbaa4ec755d4bbd29be627aa310909
GeorgiSevdalov <nojegoje@gmail.com>
2025-08-10 14:47:45 +0300

    feat: user dashboard

--- src/app/dashboard/user-dashboard/user-dashboard.ts
+++ src/app/dashboard/user-dashboard/user-dashboard.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { User } from '@supabase/supabase-js';
+import { Observable, map } from 'rxjs';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-user-dashboard',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule
+  ],
   templateUrl: './user-dashboard.html',
   styleUrl: './user-dashboard.scss'
 })
-export class UserDashboard {
+export class UserDashboard implements OnInit {
+  private authService = inject(AuthService);
+  
+  currentUser$?: Observable<User | null>;
+
+  ngOnInit(): void {
+    this.currentUser$ = this.authService.authState$.pipe(
+      map(state => state.user as User | null)
+    );
+  }
 
 }
--- src/app/shared/components/home/home.ts
+++ src/app/shared/components/home/home.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { User } from '@supabase/supabase-js';
+import { Observable, map } from 'rxjs';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -18,5 +21,17 @@ import { RouterModule } from '@angular/router';
   styleUrl: './home.scss'
 })
 export class Home {
+  private authService = inject(AuthService);
+  
+  isAuthenticated$: Observable<boolean>;
+  currentUser$: Observable<User | null>;
 
+  constructor() {
+    this.isAuthenticated$ = this.authService.authState$.pipe(
+      map(state => !!state.user)
+    );
+    this.currentUser$ = this.authService.authState$.pipe(
+      map(state => state.user as User | null)
+    );
+  }
 }
